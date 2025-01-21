83a8e00beae552104d722a471f4e3dbfd4021e03
parkkunghyun <rudgus4620@gmail.com>
2025-01-21 14:29:33 +0900

    deploy completed

--- app/components/ImageCard.jsx
+++ app/components/ImageCard.jsx
@@ -21,8 +21,9 @@ const ImageCard = ({ image }) => {
       <div className='relative'>
         <Image src={getImageUrl(image.name)} alt='image'
           className="rounded-lg w-full aspect-square"  width={200} height={200} />
+      <p className='font-bold text-center'>{image.name.split(".")[0]}</p>
         <div onClick={() => deleteFileMutation.mutate(image.name)} className='hover:scale-105 absolute  top-2 right-2'>
-        {deleteFileMutation.isPending ? <AiOutlineLoading3Quarters className="text-4xl  bg-white p-1 rounded-md"/> : <MdDeleteForever className="text-4xl bg-white p-1 rounded-md"/>}
+          {deleteFileMutation.isPending ? <AiOutlineLoading3Quarters className="text-4xl  bg-white p-1 rounded-md"/> : <MdDeleteForever className="text-4xl bg-white p-1 rounded-md"/>}
         </div>
       </div>
   )
